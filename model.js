57bce8d4315437b74dde9b44a97a5eb9746a3539
cah07r <cah07r@ecs.soton.ac.uk>
2015-11-17 18:02:11 +0000

    More Conditionals stuff - untested

--- model.js
+++ model.js
@@ -42,7 +42,88 @@ var StoryReadingList = Backbone.Collection.extend({
 var Reading = Backbone.Model.extend({
 	urlRoot: '/storyplaces/reading',
 	
+	initialize: function(){
+		var that = this;
+		var storyId = this.get("story")
+		var story = new Story({id: storyId});
+		story.fetch({
+			success: function(story){
+				that.storyObj = story;
+			}		
+		})
+	},
+	
 	getVariable: function(key){
+		var result;
+		this.get("variables").forEach(function(variable){
+			if(variable.key==key){
+				result = variable.value
+			}
+		})
+		return result
+	},
+	
+	getValue: function(val, type){
+		if(type=="String"||type="Integer"){
+			return val;
+		}
+		else if(type="Variable"){
+			return this.getVariable(val)
+		}
+			
+	},
+	
+	checkCardConditions: function(cardId){
+		var res = true;
+		var conditions = this.storyObj.getCard(cardId).conditions;
+		conditions.forEach(function(condition){
+			if(!that.checkCondition(condition)){
+				res=false;
+				break;
+			}
+		})
+		return res;
+	},
+	
+	checkCondition: function(conditionName){
+		return this.getCondition(conditionName).resolveCondition(this)
+	},
+	
+	getCondition: function(conditionName){
+		var res;
+		var conditions = this.storyObj.conditions;
+		conditions.forEach(function(condition){
+			if(condition.name==condition){
+				res=condition;
+				break;
+			}
+		})
+		
+		if(condition.type=="comparisson"){
+			return new ComparissonCondition(res)
+		}
+		else if(condition.type=="logical"){
+			return new LogicalCondition(res)
+		}
+		else{
+			return null
+		}
+	}
+	
+	
+})
+
+var ComparissonCondition = Backbone.Model.extend({
+	resolveCondition: function(context){
+		var vara = context.getValue(this.a,this.aType)
+		var varb = context.getValue(this.b,this.bType)
+		//This is a bit nasty :/
+		eval("return "+vara+" "+this.operand+" "+varb)		
+	}
+})
+
+var LogicalCondition = Backbone.Model.extend({
+	resolveCondition: function(context){
 		
 	}
 })
