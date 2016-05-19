c3588f4f64971688fa1e421fc795d4d465c04a19
Kevin Puplett <k.e.puplett@soton.ac.uk>
2016-05-19 15:57:19 +0100

    initial storySpec test

--- test/models/storySpec.js
+++ test/models/storySpec.js
@@ -1,56 +1,55 @@
 var requirejs = require('requirejs');
 var chai = require('chai');
 var expect = require('chai').expect;
-
+var sinon = require('sinon');
 
 describe('Story model', function () {
     'use strict';
-    
-    var story;
-    var cardCollection;
-    beforeEach(function (done) {
-        requirejs(['Story', 'CardCollection'], function (Story, CardCollection) {
-            story = new Story();
-            cardCollection = new CardCollection();
+
+    var Story;
+    var CardCollection;
+
+    before(function (done) {
+        requirejs(['Story', 'CardCollection'], function (StoryModel, CardCollectionModel) {
+            Story = StoryModel;
+            CardCollection = CardCollectionModel;
             done();
         });
     });
 
-    describe('when getCard is called', function(){
-        it('should return card from deck with matching id', function(){
-            
+    describe('when getCard is called', function () {
+        it('should call get() once on the deck collection with the passed ID', function () {
+
+            var story = new Story();
+            var cardCollection = new CardCollection();
+
             // arrange
-            var deck = [ { _id: 1}, { _id: 2}];                
-            story.set("deck", deck);
-            
+            story.set("deck", cardCollection);
+            var spyOnGet = sinon.spy(cardCollection, 'get');
+
             // act
-            var result = story.getCard(1);
-            
-            // assert
-            expect(result._id).to.equal(1);
+            story.getCard(2);
+
+            //assert
+            sinon.assert.calledWith(spyOnGet, 2);
+            sinon.assert.calledOnce(spyOnGet);
         });
-        
-        it('should return undefined when deck does not have card with matching id', function(){
-            var deck = [ { _id: 1}, { _id: 2}];                
-            story.set("deck", deck);
-            
-            // act
-            var result = story.getCard(3);
-            
-            // assert
-            expect(result).to.equal(undefined);
+    });
+
+    describe('when a story is fetched', function () {
+
+        it("the parse function should change a deck into a CardCollection", function() {
+            var story = new Story()
+            var data = {_id: 1, "deck": [{"content": "card 1"}, {"content": "card 2"}]};
+
+            var parsedData = story.parse(data);
+
+            var expectedCollection = new CardCollection([{"content": "card 1"}, {"content": "card 2"}]);
+
+
+
         });
-        
-        it('should return undefined when deck is empty', function() {
-            // arrange
-            story.set("deck", []);
-            
-            // act
-            var result = story.getCard(1);
-            
-            // assert
-            expect(result).to.equal(undefined);
-        });
-    });    
+
+    });
 
 });
