a60e4df1a789a9af0e333b15c95b000b3d19a6fa
Kevin Puplett <k.e.puplett@soton.ac.uk>
2016-05-20 11:15:37 +0100

    working Story class test

--- test/models/storySpec.js
+++ test/models/storySpec.js
@@ -40,12 +40,14 @@ describe('Story model', function () {
 
         it("the parse function should change a deck into a CardCollection", function() {
             var story = new Story()
-            var data = {_id: 1, "deck": [{"content": "card 1"}, {"content": "card 2"}]};
+            var data = {_id: "1", id: "1", "deck": [{"id": 1, "content": "card 1"}, {"id": 2, "content": "card 2"}]};
 
             var parsedData = story.parse(data);
 
-            var expectedCollection = new CardCollection([{"content": "card 1"}, {"content": "card 2"}]);
+            var expectedCardCollection = new CardCollection([{"id": 1, "content": "card 1"}, {"id": 2, "content": "card 2"}])
 
+            expect(diffCollectionsOnAttributes(parsedData.deck, expectedCardCollection, 'content')).to.equal(true);
+            expect(parsedData.id).to.equal("1");
 
 
         });
@@ -53,3 +55,23 @@ describe('Story model', function () {
     });
 
 });
+
+function diffCollectionsOnAttributes(collectionA, collectionB, testAttribute) {
+    if (collectionA.length != collectionB.length) {
+        return false;
+    }
+
+    var tempCollectionA = collectionA.clone();
+    var tempCollectionB = collectionB.clone();
+
+    collectionA.each(function(item) {
+        if (collectionB.findWhere({id: item.id})) {
+            if (!testAttribute || tempCollectionA.get(item.id).get(testAttribute) == tempCollectionB.get(item.id).get(testAttribute)) {
+                tempCollectionA.remove({id: item.id});
+                tempCollectionB.remove({id: item.id});
+            }
+        }
+    });
+
+    return (tempCollectionA.length == 0 && tempCollectionB.length == 0)
+}
