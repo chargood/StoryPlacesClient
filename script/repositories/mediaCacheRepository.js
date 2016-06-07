7bdce508026dfa1ce4c76270d8cfeebce13fbaa5
David Pepper <d.pepper@soton.ac.uk>
2016-06-07 14:41:22 +0100

    Only clears images from the local cache

--- script/repositories/mediaCacheRepository.js
+++ script/repositories/mediaCacheRepository.js
@@ -2,16 +2,21 @@ define(['CacheManager', 'underscore', 'jquery'], function (CacheManager, _, $) {
     'use strict';
 
     var cacheFull = false;
-
-    var buildMediaKey = function (storyId, imageId) {
-        return storyId + ':' + imageId;
+    var keyPrefix = "cache-manager:";
+    
+    var buildStoryKey = function(storyId) {
+        return keyPrefix + storyId;
+    };
+    
+    var buildMediaKey = function (storyId, mediaId) {
+        return buildStoryKey(storyId) + ':' + mediaId;
     };
 
-    var buildUrl = function (image) {
-        return 'story/' + image.storyId + '/media/' + image.mediaId;
+    var buildUrl = function (storyId, mediaId) {
+        return 'storyplaces/story/' + storyId + '/media/' + mediaId;
     };
 
-    var buildSettingsObject = function (image) {
+    var buildSettingsObject = function () {
         return {
             method: 'GET',
             headers: {
@@ -19,40 +24,45 @@ define(['CacheManager', 'underscore', 'jquery'], function (CacheManager, _, $) {
             }
         };
     };
-    
-    var populateCache = function (images) {
 
-        // clear any existing cache
-        CacheManager.clear();
+    var populateCache = function (storyId, mediaIds) {
+
+        // clear any existing cache for other stories
+        var storyKey = buildStoryKey(storyId);
+        CacheManager.clear(keyPrefix, storyKey);
         cacheFull = false;
 
-        _.each(images, function (image) {
+        _.each(mediaIds, function (mediaId) {
 
-            var key = buildMediaKey(image.storyId, image.mediaId);
+            var key = buildMediaKey(storyId, mediaId);
 
-            var settings = buildSettingsObject(image);
-            var url = buildUrl(image);
-            $.ajax(url, settings)
-                .done(function (data) {
-                    if (!cacheFull) {
-                        try {
-                            CacheManager.set(key, data);
-                        } catch (exception) {
-                            if (exception.name === 'QuotaExceededError' || exception.name === 'NS_ERROR_DOM_QUOTA_REACHED') {
-                                cacheFull = true;
+            if (CacheManager.get(key) == undefined) {
+                
+                var settings = buildSettingsObject(mediaId);
+                var url = buildUrl(storyId, mediaId);
+                
+                $.ajax(url, settings)
+                    .done(function (data) {
+                        if (!cacheFull) {
+                            try {
+                                CacheManager.set(key, data);
+                            } catch (exception) {
+                                if (exception.name === 'QuotaExceededError' || exception.name === 'NS_ERROR_DOM_QUOTA_REACHED') {
+                                    cacheFull = true;
+                                }
                             }
                         }
-                    }
-                })
-                .fail(function (err) {
-                    console.log("Failed to get media storyId:" + image.storyId + " mediaId:" + image.mediaId);
-                });
-
+                    })
+                    
+                    .fail(function (err) {
+                        console.log("Failed to get media storyId:" + storyId + " mediaId:" + mediaId);
+                    });
+            }
         });
     };
 
-    var getCacheItem = function (storyId, imageId) {
-        var key = buildMediaKey(storyId, imageId);
+    var getCacheItem = function (storyId, mediaId) {
+        var key = buildMediaKey(storyId, mediaId);
         return CacheManager.get(key);
     };
 
--- script/utils/localStorageCacheManager.js
+++ script/utils/localStorageCacheManager.js
@@ -1,21 +1,34 @@
-define([], function(){
+define(['underscore'], function () {
     'use strict';
-    
+
     // represents an abstraction layer over the caching mechanism.
-    
-    var clear = function() {
-        localStorage.clear();
+
+    var clear = function (keyMustStartWith, keyMustNotStartWith) {
+
+       for (var index = 0; index < localStorage.length; index++) {
+            var key = localStorage.key(index);
+            
+            if (keyMustStartWith != undefined && !key.startsWith(keyMustStartWith)) {
+                continue;
+            }
+            
+            if (keyMustNotStartWith != undefined && key.startsWith(keyMustNotStartWith)) {
+                continue;
+            }
+            
+            localStorage.removeItem(key);
+        }
     };
-    
-    var set = function(key, value) {
+
+    var set = function (key, value) {
         localStorage.setItem(key, value);
     };
-    
-    var get = function(key){
-        localStorage.getItem(key);
+
+    var get = function (key) {
+        return localStorage.getItem(key);
     };
-    
-    return {        
+
+    return {
         set: set,
         clear: clear,
         get: get
